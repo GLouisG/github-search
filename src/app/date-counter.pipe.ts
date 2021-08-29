24cfd902ee7a0611aef9d07fd091f4459977bbf0
GLouisG <louis.gacho@student.moringaschool.com>
2021-08-29 16:05:32 +0300

    add logic for calculation of days ago using date to dateCounter pipe

--- src/app/date-counter.pipe.ts
+++ src/app/date-counter.pipe.ts
@@ -5,8 +5,29 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCounterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+   
+
+   let createYr = value.slice(0, -16);
+  ;
+   
+   let createMonth = value.slice(5, -13)
+  
+
+   let createDate = value.slice(8, -10)
+  
+
+   let createMoment:any = new Date(createYr, createMonth-1, createDate);
+   
+
+   let today:any = new Date();
+
+   var difference=Math.abs(createMoment-today);
+   var diffInSeconds = difference*0.001;
+   const seconds = 86400;
+   var daysSince = Math.floor(diffInSeconds/seconds);
+   
+   return daysSince;
   }
 
 }
